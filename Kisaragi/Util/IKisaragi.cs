97add29cc13f1c56616a2361dec044b29bf7a62f
Asterisk <kuro5858tube@gmail.com>
2018-03-13 21:12:41 +0900

    [wip] Added Interface for IKisaragi.

---
+++ Kisaragi/Util/IKisaragi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kisaragi.Util
+{
+	/// <summary>
+	/// Kisaragi にて使用するイベントを提供します。
+	/// </summary>
+	public interface IKisaragi
+	{
+		/// <summary>
+		/// 時間が経過し、変化したタイミングを監視するイベントハンドラ
+		/// 現状は、1時間
+		/// </summary>
+		event EventHandler<Utils<int>> MonitoringTimeChanged;
+	}
+}
